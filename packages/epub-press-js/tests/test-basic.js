4602abb39875a7a02c8a5da56d5abc2e407a892e
Proful <profulla.sadangi@thomsonreuters.com>
2018-08-29 17:35:16 +1000

    Trying to run in windows. don't know what's going on

---
+++ packages/epub-press-js/tests/test-basic.js
@@ -0,0 +1,19 @@
+const EpubPress = require('../build/index');
+
+const book = new EpubPress({
+    title: 'A book',
+    description: 'A book',
+    urls: [
+        'http://www.cbc.ca/news/canada/toronto/penny-oleksiak-coach-1.3712572'
+    ]
+});
+
+book.publish().then(() => {
+    return book.download();
+}).then(() => {
+    console.log('done');
+}).catch(console.error);
+
+book.on('statusUpdate', (status) => {
+    console.log(status);
+});
